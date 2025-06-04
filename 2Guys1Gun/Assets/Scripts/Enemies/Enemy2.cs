629da58d9a8fa9af32e4e4755b87da7d5a0e8915
Issiins <160136271+Issiins@users.noreply.github.com>
2025-06-04 15:38:29 +0300

    Revert "Fixed commit"
    This reverts commit 7703f8dfee27dc4a3c61e13fc98930a856ad8c49.

--- 2Guys1Gun/Assets/Scripts/Enemies/Enemy2.cs
+++ 2Guys1Gun/Assets/Scripts/Enemies/Enemy2.cs
@@ -54,16 +54,4 @@ public class Enemy2 : MonoBehaviour
             Debug.Log("Enemy hit by projectile and destroyed!");
         }
     }
-    void OnDestroy()
-    {      
-            GameObject soulCounterObject = GameObject.FindGameObjectWithTag("SoulCounter");
-            if (soulCounterObject != null)
-            {
-                SoulCounterHandler counter = soulCounterObject.GetComponent<SoulCounterHandler>();
-                if (counter != null)
-                {
-                    counter.UpdateSoulCounter();
-                }
-            }      
-    }
 }
